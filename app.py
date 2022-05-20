25bf709352da77195c6713a138a41398aaa3cde1
Pratush <pratushrai0309@gmail.com>
2022-05-20 18:31:17 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "hello"
 
-@app.route('/')
+#Form Class 
+class NameForm(FlaskForm):
+  name = StringField('Name', validators=[DataRequired()])
+  submit = SubmitField('Submit')
+
+@app.route('/',methods=("POST",'GET'))
 def home():
-  return {"message":"Hello World!"}
+  name = None
+  form = NameForm()
+  if form.validate_on_submit():
+    name = form.name.data
+    form.name.data = ''
+  return render_template('home.html',form=form, name=name)
