26eee99440ab9304b737d0c4ba7e0fc8a8fb4011
Pratush <pratushrai0309@gmail.com>
2022-05-20 21:58:20 +0530

    Error handling improved

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SECRET_KEY'] = "hello"
 #Form Class 
 class NameForm(FlaskForm):
   name = StringField('Name of the city', validators=[DataRequired()])
-  submit = SubmitField('Submit')
+  submit = SubmitField('Search 🔍')
 
 @app.route('/',methods=("POST",'GET'))
 def home():
@@ -19,11 +19,13 @@ def home():
   temp = None
   if form.validate_on_submit():
     name = form.name.data
-    r = None
-    try:
-      r = requests.get(f'https://api.weatherapi.com/v1/current.json?key=49da65cf146f4636a0b181834221604&q={name}&aqi=no').json()
-    except:
-      r = {'current':{'temp_c' : 'An Error Occured'}}
-    temp = r['current']['temp_c']
-    form.name.data = ''
+    r = requests.get(f'https://api.weatherapi.com/v1/current.json?key=49da65cf146f4636a0b181834221604&q={name}&aqi=no').json()
+    if not 'error' in r:
+      temp = r['current']['temp_c']
+      form.name.data = ''
+
+    else:
+      temp = r['error']['message']
+      form.name.data = ''
+      
   return render_template('home.html',form=form, name=name,temp = temp)
